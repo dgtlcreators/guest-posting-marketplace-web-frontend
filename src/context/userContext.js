3c935ce8178f7581a0ba9ccb6ffcf001bf729616
dgtlcreators <rojadgtlmart@gmail.com>
2024-11-11 18:22:58 +0530

    Chnage Url

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -68,9 +68,10 @@ try {
    // return storedUser ? JSON.parse(storedUser) : null;
   });*/
 
-//const localhosturl="http://localhost:5000"
+const localhosturl="http://localhost:5000"
 //const localhosturl="https://guest-posting-marketplace-web-backend.onrender.com"
-const localhosturl="https://guest-posting-marketplace-web-backend-1.onrender.com"
+//const localhosturl="https://guest-posting-marketplace-web-backend-1.onrender.com"
+//const localhosturl="https://guest-posting-marketplace-web-backend-2.onrender.com"
 
 
 const [loading, setLoading] = useState(true);
